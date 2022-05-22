f4ab70404bcc6bcc3b039de9138a8bc6e45d614e
bocharnikovv <ivanbocharnikov1@gmail.com>
2022-05-22 21:24:26 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector("#burger");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(true);
+const body = document.body;
+
+burger.addEventListener("click", burgerHandler);
+
+function burgerHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    burger.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
